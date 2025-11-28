8a33ac28769f833e9c9f25d4e307e31c64a8a3b6
Venkata Nitish Gupta Arisetty <venkata.arisetty@q3tech.com>
2025-11-28 19:28:41 +0530

    Changes

--- app/src/androidTest/java/ajitsingh/com/expensemanager/activity/MainActivityTest.java
+++ app/src/androidTest/java/ajitsingh/com/expensemanager/activity/MainActivityTest.java
@@ -13,7 +13,6 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
     @Rule
